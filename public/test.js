6178017f7899cb342d2593d313011f45f9bd53f0
bstephe9 <tun78906@temple.edu>
2025-01-03 05:57:17 -0500

    Make it work with React

--- public/test.js
+++ public/test.js
@@ -12,7 +12,10 @@
 // after the generated code, you will need to define   var Module = {};
 // before the code. Then that object will be used in the code, and you
 // can continue to use Module afterwards as well.
-var Module = typeof Module != 'undefined' ? Module : {};
+var Module = {
+  canvas: document.getElementById("canvas"), // Use React's canvas
+};
+
 
 // Determine the runtime environment we are in. You can customize this by
 // setting the ENVIRONMENT setting at compile time (see settings.js).
@@ -597,7 +600,7 @@ function createExportWrapper(name, nargs) {
 // include: runtime_exceptions.js
 // end include: runtime_exceptions.js
 function findWasmBinary() {
-    var f = 'index.wasm';
+    var f = 'test.wasm';
     if (!isDataURI(f)) {
       return locateFile(f);
     }
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    // Dynamically load the SDL2 WebAssembly application
+    const script = document.createElement("script");
+    script.src = "/test.js"; // Path to the compiled JavaScript glue code
+    script.async = true;
+
+    // Append the script to the document
+    document.body.appendChild(script);
+
+    return () => {
+      // Cleanup the script on component unmount
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Vite + React + SDL2 Example</h1>
+      {/* Placeholder for SDL2 rendering */}
+      <div
+        id="sdl2-container"
+        style={{
+          width: "800px",
+          height: "600px",
+          border: "1px solid black",
+        }}
+      >
+        <canvas id="canvas" width="800" height="600"></canvas>
+      </div>
+    </div>
+  );
+}
+
+export default App;
