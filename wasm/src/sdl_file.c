99dce9075de5c6a423776ed764c8b243ac129e32
bstephe9 <tun78906@temple.edu>
2025-01-05 00:15:50 -0500

    Change SDL.h paths

--- wasm/src/sdl_file.c
+++ wasm/src/sdl_file.c
@@ -1,4 +1,4 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
--- wasm/src/test.c
+++ wasm/src/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 #include "sdl_file.h"
 
